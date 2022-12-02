b92a0fb4f9570ed8146a82ca6603205850086374
John Pablo Valverde <john.davis.2015.03@gmail.com>
2022-12-02 01:59:01 -0500

    added react toastify

--- src/layouts/auth/AuthLayout.jsx
+++ src/layouts/auth/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export const AuthLayout = () => {
   return (
-    <div className="bg-app-white min-h-screen flex items-center justify-center">
+    <div className="bg-app-white min-h-screen flex items-center justify-center p-5">
       <Outlet />
     </div>
   );
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,36 +1,74 @@
+import { useState } from "react";
+import { RiEyeLine, RiEyeOffLine, RiLockLine } from "react-icons/ri";
 // import { RiMailLine } from "react-icons/ri";
-import { RiLockLine } from "react-icons/ri";
 import logoYard from "../../../public/assets/logos/logo_yard_sale.svg";
 
 export const Login = () => {
+  const [showPasswordFirst, setShowPasswordFirst] = useState(false);
+  const [showPasswordSecond, setShowPasswordSecond] = useState(false);
+
+  const handleShowPasswordFirst = () => {
+    setShowPasswordFirst(!showPasswordFirst);
+  };
+  const handleShowPasswordSecond = () => {
+    setShowPasswordSecond(!showPasswordSecond);
+  };
+
   return (
-    <div className="p-4">
-      <img src={logoYard} alt="logo_yard_sale" className="" />
-      <h1 className="text-lg font-bold text-center">Create a new password</h1>
-      <p className="text-lg text-app-silver">
+    // Aqui va un p-5 que lo tiene el AuthLayout para todos los formularios o paginas.
+    <div className="md:w-96 w-full">
+      <img src={logoYard} alt="logo_yard_sale" className="mx-auto w-36" />
+      <h1 className="text-lg font-bold text-center mt-16">
+        Create a new password
+      </h1>
+      <p className="text-lg text-app-silver mt-2">
         Enter a new password for your account
       </p>
 
-      <form className="flex flex-col gap-4">
-        <div className="relative">
-          <label className="text-sm font-bold">Password</label>
+      <form className="flex flex-col">
+        <label className="text-sm font-bold mb-1 mt-10">Password</label>
+        <div className="relative mb-6">
           <RiLockLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
           <input
-            type="password"
-            className="bg-app-alabaster w-full rounded-lg py-2 px-8 outline-none"
+            type={showPasswordFirst ? "text" : "password"}
+            className="bg-app-alabaster w-full rounded-lg py-2 px-8 outline-none border border-gray-200"
             placeholder="************"
           />
+          {showPasswordFirst ? (
+            <RiEyeOffLine
+              onClick={handleShowPasswordFirst}
+              className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"
+            />
+          ) : (
+            <RiEyeLine
+              onClick={handleShowPasswordFirst}
+              className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"
+            />
+          )}
         </div>
-        <div className="relative">
-          <label className="text-sm font-bold">Re-enter password</label>
+        <label className="text-sm font-bold mb-1">Re-enter password</label>
+        <div className="relative mb-6">
+          <RiLockLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
           <input
-            type="password"
-            className="bg-app-alabaster w-full rounded-lg py-2 px-4 outline-none"
+            type={showPasswordSecond ? "text" : "password"}
+            className="bg-app-alabaster w-full rounded-lg py-2 px-8 outline-none border border-gray-200"
             placeholder="************"
           />
+          {showPasswordSecond ? (
+            <RiEyeOffLine
+              onClick={handleShowPasswordSecond}
+              className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"
+            />
+          ) : (
+            <RiEyeLine
+              onClick={handleShowPasswordSecond}
+              className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"
+            />
+          )}
         </div>
         <div>
-          <button className="bg-app-green text-white w-full rounded-lg py-2 mt-5">
+          {/* <button className="bg-app-green text-white w-full rounded-lg py-2 mt-2 hover:scale-105 transition-all"> */}
+          <button className="bg-app-green text-white w-full rounded-lg py-4 mt-2 hover:bg-green-800 transition-colors">
             Confirm
           </button>
         </div>
