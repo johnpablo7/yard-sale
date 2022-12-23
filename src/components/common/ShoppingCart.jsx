be0705324a127a07026a0e8c8723f568f8a27976
John Pablo <John Pablo Valverde>
2022-12-22 19:58:42 -0500

    add gists

--- src/components/common/ShoppingCart.jsx
+++ src/components/common/ShoppingCart.jsx
@@ -6,24 +6,30 @@ import close from "../../../public/assets/icons/icon_close.svg";
 
 export const ShoppingCart = () => {
   return (
-    <div className="fixed w-[384px] h-auto rounded drop-shadow top-16 md:right-0 bg-app-white border-app-white outline-none">
-      <div>
-        <button className="bg-app-white text-3xl p-3 left-4 top-6">
+    <div className="fixed w-[384px] h-auto rounded drop-shadow top-16 md:right-0 bg-app-white border-app-white outline-none py-6 px-4">
+      <div className="flex items-center gap-6 pl-2">
+        <button className="text-3xl">
           <img src={arrowLeft} alt="arrow" />
         </button>
-        <p>Shopping cart</p>
+        <p className="text-lg font-bold">Shopping cart</p>
       </div>
 
-      <div className="my-3">
-        <img src={image1} alt="image1" />
-        <p>Round shelf</p>
+      <div className="flex items-center text-center mt-9 mb-20">
+        <img
+          src={image1}
+          alt="image1"
+          className="mb-6 mr-4"
+          width={77}
+          height={77}
+        />
+        <p className="text-sm text-app-boulder">Round shelf</p>
         <p>$ 120,00</p>
         <button className="bg-app-white text-3xl rounded-full p-3 left-4 top-6">
           <img src={close} alt="close" />
         </button>
       </div>
 
-      <div className="flex flex-col px-9 py-5">
+      <div className="flex flex-col">
         <div>
           <p>Total</p>
           <p>$ 560.00</p>
